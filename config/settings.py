0b65ead47c15883069ca5379701b3d13a40bc19f
Sherzod <mustafo.nurhayo@gmail.com>
2023-02-19 15:13:52 +0500

    qwerty

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h-@e$oe#iio&$9i58hmn8v2=-@9imp88o1&$qs8dq8r$i8r*&+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
